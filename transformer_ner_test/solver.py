33a68acbfff8ff3910a0ad2977dd33da2aa29242
Kao Pei-Wei <tif135@gmail.com>
2020-05-09 20:23:20 +0800

    ner_test

--- transformer_ner_test/solver.py
+++ transformer_ner_test/solver.py
@@ -171,7 +171,7 @@ class Solver():
             for i in range(out.size(0)):
                 nonz = torch.nonzero(batch['src_mask'][i])
                 #print(nonz)
-                idx = nonz[-1][1].item()
+                idx = nonz[-1][1].item()+1
                 print('non zero idx:', idx)
                 sentence = self.data_utils.id2label(out[i][:idx], True)
                 #print(l[1:])
